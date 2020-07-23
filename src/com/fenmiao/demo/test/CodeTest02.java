d7ca58c660085665cec3d3e940399d8d71838f52
Administrator <chw410184@162.com>
2020-07-23 10:46:04 +0800

    今天的测试提交

--- src/com/fenmiao/demo/test/CodeTest02.java
+++ src/com/fenmiao/demo/test/CodeTest02.java
@@ -3,10 +3,21 @@ package com.fenmiao.demo.test;
 
 import com.fenmiao.demo.entity.Result;
 
+import java.util.regex.Pattern;
+
 public class CodeTest02 {
     public static void main(String[] args) {
-        Result result = new Result();
 
-        System.out.println(result);
+        System.out.println(System.currentTimeMillis());
+        System.out.println((long) (Math.random() * (10)));
+        System.out.println(System.currentTimeMillis() + (long) (Math.random() * (30)));
+
+        System.out.println(isBase64("chw"));
+        System.out.println("99999"+"\n"+"((");
+    }
+
+    private static boolean isBase64(String str) {
+        String base64Pattern = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$";
+        return Pattern.matches(base64Pattern, str);
     }
 }
