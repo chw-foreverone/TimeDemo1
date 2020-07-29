03124609055a516ce197108b15aba5e611d2bc60
chw-foreverone <chw410184@163.com>
2020-07-29 13:32:08 +0800

    今天的测试提交2

--- src/com/fenmiao/demo/test/CodeTest03.java
+++ src/com/fenmiao/demo/test/CodeTest03.java
@@ -13,11 +13,24 @@ public class CodeTest03 {
 
 
         List<User> list = new ArrayList<>();
+        User user1 = new User("127", 2);
+        User user2 = new User("126", 4);
+        User user3 = new User("124", 2);
+        User user4 = new User("121", 2);
+        User user5 = new User("123", 5);
+        User user6 = new User("121", 3);
+
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        list.add(user4);
+        list.add(user5);
+        list.add(user6);
         HashMap<Object, Object> map = new HashMap<>();
         list.forEach(a -> {
-            for (int i = 0; i < list.size(); i++) {
-                if (a.getName().equals(list.get(i).getName())) {
-                    if (a.getScore() > list.get(i).getScore()) {
+            for (User user : list) {
+                if (a.getName().equals(user.getName())) {
+                    if (a.getScore() > user.getScore()) {
                         map.put(a.getName(), a.getScore());
                     }
                 }
